c084870f84850bf1a24f6df9b9e4baacbed38869
Chocolawt <30875154+Chocolawt@users.noreply.github.com>
2017-08-28 08:43:04 -1000

    Create LNcarter.js

---
+++ LNcarter.js
@@ -0,0 +1,11 @@
+var x = prompt("enter firstName, lastName, age, or grade")
+
+
+var students = {
+		firstName: "Carter",
+		lastname: "Suen",
+    age: 15,
+    grade: 11
+
+};
+window.alert(students[x]);
