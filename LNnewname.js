6180fd789f6a047b3294a86bf4e6170bd5dd6b51
Chocolawt <30875154+Chocolawt@users.noreply.github.com>
2017-08-28 08:43:33 -1000

    Create LNnewname.js

---
+++ LNnewname.js
@@ -0,0 +1,10 @@
+var newName = prompt("What is his new first name?");
+
+var identity = {
+		firstName: "Carter",
+		lastname: "Suen",
+    age: 15,
+    grade: 11
+
+};
+window.alert(identity.firstName = newName);
