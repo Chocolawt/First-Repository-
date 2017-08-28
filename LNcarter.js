8bebe956c09fe0142ea273fea2c38e49009ab313
Chocolawt <30875154+Chocolawt@users.noreply.github.com>
2017-08-28 08:47:04 -1000

    Update LNcarter.js

--- LNcarter.js
+++ LNcarter.js
@@ -9,3 +9,17 @@ var students = {
 
 };
 window.alert(students[x]);
+
+*/ Edited to change his first name into another name*/
+
+var newName = prompt("What is his new first name?");
+
+var identity = {
+		firstName: "Carter",
+		lastname: "Suen",
+    age: 15,
+    grade: 11
+
+};
+window.alert(identity.firstName = newName);
+
