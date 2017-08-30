fa41e11ba6c7cb4203cbb541d283f1e174d4bc88
Chocolawt <30875154+Chocolawt@users.noreply.github.com>
2017-08-30 08:49:43 -1000

    Update object.js

--- object.js
+++ object.js
@@ -1,5 +1,5 @@
 var bob = {name: "bob", age: 10, grade: 5, friends: ["Jack", "John", "Jesse"]};
-var frank = {name" "frank", age: 11, grade: 6, friends: ["Kelly", "Kari", "Kai"]};
+var frank = {name: "frank", age: 11, grade: 6, friends: ["Kelly", "Kari", "Kai"]};
 var steve {name: "steve", age: 12, grade: 7, friends: ["Randy", "Ralph", "Roger"]};
 
 var x = [bob, frank, steve];
