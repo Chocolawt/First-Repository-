b3065a54d18bee5aceba40475c56e643bc46fc33
Chocolawt <30875154+Chocolawt@users.noreply.github.com>
2017-08-30 08:30:07 -1000

    Create object.js

---
+++ object.js
@@ -0,0 +1,7 @@
+var bob = {name: "bob", age: 10, grade: 5, friends: ["Jack,"John", "Jesse"]};
+var frank = {name" "frank", age: 11,grade: 6, friends: ["Kelly", "Kari", "Kai"]};
+var steve {name: "steve", age: 12, grade: 7, friends: ["Randy", "Ralph", "Roger"]};
+
+var x = [bob, frank, steve];
+
+window.alert(x[1].friends[0]);
